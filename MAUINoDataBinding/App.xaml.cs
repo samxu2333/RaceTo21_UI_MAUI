296c098ad632f4190c2f7684f05848cc1c96ab6e
samxu2333 <157269006+samxu2333@users.noreply.github.com>
2024-03-13 02:08:37 +0000

    Add project files.

---
+++ MAUINoDataBinding/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MAUINoDataBinding;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		// By changing this to instantiate MainPage instead of AppShell,
+		// we remove one layer of complexity.Now whatever we put on MainPage
+		// is what we control, with no extra containers around it.
+		//
+		// For this demo, I have deleted AppShell, but you can also simply
+		// ignore it.
+		MainPage = new MainPage();
+	}
+}
+
---
+++ MAUINoDataBinding/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUINoDataBinding;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
